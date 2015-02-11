d4dad0514fe82d6def2af3686c32808d171f9c99
jmarcher <joacorock@gmail.com>
2015-02-11 14:57:23 +0100

    Coso

---
+++ database/migrations/2015_02_10_144759_turnoRepartir.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TurnoRepartir extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('games', function(Blueprint $table)
+		{
+			$table->boolean("seDebeRepartir")->default(true);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('games', function(Blueprint $table)
+		{
+			$table->dropColumn("seDebeRepartir");
+		});
+	}
+
+}
